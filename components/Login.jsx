77756b1cfecf984e7400ec0107d7ad730a55126a
Aaditya <webdeveloperaaditya@gmail.com>
2023-04-27 13:27:43 +0530

    Final Submission

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
+import { useRouter } from "next/router";
+
 import { Checkbox, Input, useInput, Button, Spacer } from "@nextui-org/react";
 import Image from 'next/image'
 import mypic from "../public/register.jpg"
@@ -7,11 +9,15 @@ import mypic from "../public/register.jpg"
 
 export default function Login() {
   const { value, reset, bindings } = useInput("");
-  
+
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+
   const validateEmail = (value) => {
     return value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
   };
 
+  const router = useRouter();
   const helper = React.useMemo(() => {
     if (!value)
       return {
@@ -24,6 +30,26 @@ export default function Login() {
       color: isValid ? "success" : "error",
     };
   }, [value]);
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    var passwordval = document.getElementById("password").value;
+    var emailval = document.getElementById("email").value;
+
+    // Check if the email and password match the expected values
+    if (emailval === "Aaditya@gmail.com" && passwordval === "Aaditya@1234") {
+      // alert("Credentials are correct. Redirecting to dashboard...");
+      router.push("/Liveclass");
+      setLoggedIn(true);
+      localStorage.setItem("loggedIn", true);
+
+      // You can redirect the user to the dashboard page here
+    } else {
+      alert("Incorrect credentials. Please try again.");
+    }
+  };
   return (
     <div className="main-wrapper flex justify-evenly my-14">
     <div className="leftcol ">
@@ -33,7 +59,7 @@ export default function Login() {
     height={600}
     />
     </div>
-    <form className="w-4/12 register_form p-10 bg-gray-300 rounded-2xl">
+    <form onSubmit={handleSubmit} className="w-4/12 register_form p-10 bg-gray-300 rounded-2xl">
       <div className="text-2xl font-bold text-center	">Login</div>
     <Input
           {...bindings}
@@ -46,9 +72,11 @@ export default function Login() {
           // helperText={helper.text}
           type="email"
           label="Email"
+          id="email"
           fullWidth={true}
           placeholder="Enter your email"
           required={true}
+          // onChange={(e) => setEmail(e.target.value)}
           autoComplete
         /> <Spacer />
         
@@ -59,9 +87,11 @@ export default function Login() {
           // helperText="Insecure password"
           type="password"
           label="Password"
+          id="password"
           required={true}
           placeholder="Enter your password"
           fullWidth={true}
+          // onChange={(e) => setPassword(e.target.value)}
           
         /><Spacer />
         
--- src/pages/Liveclass.jsx
+++ src/pages/Liveclass.jsx
@@ -1,7 +1,7 @@
 import Navbar1 from '../../components/header'
 import Footer from '../../components/footer'
 import Split from 'react-split';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Textarea, Spacer, Button } from '@nextui-org/react';
 
 
@@ -9,6 +9,10 @@ export default function Liveclass() {
 
   const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
+
+  // useEffect(() => {
+  //   const loggedIn = localStorage.getItem("loggedIn");
+  // }, []);
 
   const executeCode = () => {
     try {
@@ -29,38 +33,40 @@ export default function Liveclass() {
     setOutput("");
   };
   
-    return (
-     <>
-     <div>
-     <Navbar1/>
-     <Split className={`h-auto flex flex-row m-10`}>
-     <iframe className='w-6/12' width="auto" height="auto" src="https://www.youtube.com/embed/W6NZfCO5SIk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-     <div className="block w-3/12">
-      <Textarea 
-      minRows={12}
-      maxRows={12}
-      fullWidth={true}
-        bordered
-        labelPlaceholder="Enter your code here"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></Textarea> <Spacer />
-      <Button color="success" auto onClick={executeCode}>Run</Button> </div> 
-      <div className="block w-3/12">
-      <Textarea
-      minRows={12}
-      maxRows={12}
-      fullWidth={true}
-        placeholder="Output will be displayed here"
-        value={output}
-        readOnly
-      ></Textarea><Spacer />
-      <Button color="warning" auto onClick={clearOutput}>Clear</Button>
-    </div>
-     </Split>
-     <Footer />
-     </div>
-     </>
-    )
-  }
+    // if(loggedIn) {
+      return (
+        <>
+        <div>
+        <Navbar1/>
+        <Split className={`h-auto flex flex-row m-10`}>
+        <iframe className='w-6/12' width="auto" height="auto" src="https://www.youtube.com/embed/W6NZfCO5SIk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div className="block w-3/12">
+         <Textarea 
+         minRows={12}
+         maxRows={12}
+         fullWidth={true}
+           bordered
+           labelPlaceholder="Enter your code here"
+           value={input}
+           onChange={(e) => setInput(e.target.value)}
+         ></Textarea> <Spacer />
+         <Button color="success" auto onClick={executeCode}>Run</Button> </div> 
+         <div className="block w-3/12">
+         <Textarea
+         minRows={12}
+         maxRows={12}
+         fullWidth={true}
+           placeholder="Output will be displayed here"
+           value={output}
+           readOnly
+         ></Textarea><Spacer />
+         <Button color="warning" auto onClick={clearOutput}>Clear</Button>
+       </div>
+        </Split>
+        <Footer />
+        </div>
+        </>
+       )
+    }
+  // }
   
